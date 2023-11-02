da1e3d0390a7a7892abd41b5288999a8950b4b8b
Jose Marchant <69648244+JoMarchant@users.noreply.github.com>
2023-11-01 21:03:11 -0300

    Crea la app

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Application } from "express";
+import dotenv from "dotenv";
+
+import paymentRoutes from "./routes/payment";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/payment", paymentRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Express server is listening at ${PORT}`);
+});
