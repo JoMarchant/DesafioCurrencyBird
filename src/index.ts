72b895b6867fa27f076239cf16e84633ac1f0c56
Jose Marchant <69648244+JoMarchant@users.noreply.github.com>
2023-11-01 23:13:17 -0300

    urlencoding

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/payment", paymentRoutes);
 
